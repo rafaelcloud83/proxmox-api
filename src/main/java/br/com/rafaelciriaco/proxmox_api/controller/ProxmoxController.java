2709c84bbb978cb42ac9bfdfb2113a1c0d488d5e
rafaelcloud83 <rafaelcloud83@gmail.com>
2026-03-15 19:12:02 -0300

    alterado crossorigin

--- src/main/java/br/com/rafaelciriaco/proxmox_api/controller/ProxmoxController.java
+++ src/main/java/br/com/rafaelciriaco/proxmox_api/controller/ProxmoxController.java
@@ -3,10 +3,12 @@ package br.com.rafaelciriaco.proxmox_api.controller;
 import br.com.rafaelciriaco.proxmox_api.model.Proxmox;
 import br.com.rafaelciriaco.proxmox_api.service.ProxmoxService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*")
 @RestController()
 @RequestMapping("/api")
 public class ProxmoxController {
