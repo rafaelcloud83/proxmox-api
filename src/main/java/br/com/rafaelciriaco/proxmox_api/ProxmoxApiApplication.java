368b6590b49db5e281ceeb489102c1cb459697be
rafaelcloud83 <rafaelcloud83@gmail.com>
2026-03-15 16:47:05 -0300

    first commit

---
+++ src/main/java/br/com/rafaelciriaco/proxmox_api/ProxmoxApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.rafaelciriaco.proxmox_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxmoxApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxmoxApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rafaelciriaco/proxmox_api/ProxmoxApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rafaelciriaco.proxmox_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxmoxApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
